b0a0148a421b758204aa1803ca90d1bdc13dce88
Dmitrii Fishchenko <dmitrii.fishchenko@soti.net>
2018-02-15 17:18:36 -0500

    bla

---
+++ Soti.LogReader/Log/LogFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soti.LogReader.Model;
+
+namespace Soti.LogReader.Log
+{
+    public class LogFile
+    {
+        public ComponentType Type { get; set; }
+        public FileInfo FileInfo { get; set; }
+    }
+}
